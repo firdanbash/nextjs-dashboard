8d4a7aae126f0a31c7a11998194cf4c44f073d31
firdanbash <firdana.id@gmail.com>
2025-08-11 22:14:27 +0700

    deploy

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -80,6 +80,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
+    console.error(error); 
     // If a database error occurs, return a more specific error.
     return {
       message: 'Database Error: Failed to Create Invoice.',
